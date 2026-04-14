431153e4a0cf5dc67a1d4af827d9134ee50036d3
cjtec2025-jpg <cjtec2025@gmail.com>
2026-04-14 14:00:58 -0300

    Update extrato_screen.dart

--- lib/extrato_screen.dart
+++ lib/extrato_screen.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
 
-class ExtratoScreen extends StatelessWidget {
+class ExtratoScreen extends StatefulWidget {
   const ExtratoScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final movimentacoes = [
-      {
-        'tipo': 'entrada',
-        'valor': 'R\$ 300,00',
-        'categoria': 'Serviços',
-        'descricao': 'Conserto notebook',
-      },
-      {
-        'tipo': 'saida',
-        'valor': 'R\$ 50,00',
-        'categoria': 'Transporte',
-        'descricao': 'Uber cliente',
-      },
-      {
-        'tipo': 'entrada',
-        'valor': 'R\$ 120,00',
-        'categoria': 'Vendas',
-        'descricao': 'Produto X',
-      },
-    ];
+  State<ExtratoScreen> createState() => _ExtratoScreenState();
+}
 
+class _ExtratoScreenState extends State<ExtratoScreen> {
+  final List<Map<String, String>> movimentacoes = [
+    {
+      'tipo': 'entrada',
+      'valor': 'R\$ 300,00',
+      'categoria': 'Serviços',
+      'descricao': 'Conserto notebook',
+    },
+    {
+      'tipo': 'saida',
+      'valor': 'R\$ 50,00',
+      'categoria': 'Transporte',
+      'descricao': 'Uber cliente',
+    },
+    {
+      'tipo': 'entrada',
+      'valor': 'R\$ 120,00',
+      'categoria': 'Vendas',
+      'descricao': 'Produto X',
+    },
+  ];
+
+  void _excluirItem(int index) {
+    setState(() {
+      movimentacoes.removeAt(index);
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Registro excluído com sucesso'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Extrato'),
@@ -44,12 +61,12 @@ class ExtratoScreen extends StatelessWidget {
                 isEntrada ? Icons.add_circle : Icons.remove_circle,
                 color: isEntrada ? Colors.green : Colors.red,
               ),
-              title: Text(item['valor']!),
+              title: Text(item['valor'] ?? ''),
               subtitle: Text(
                 '${item['categoria']} - ${item['descricao']}',
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () => _excluirItem(index),
                 icon: const Icon(Icons.delete),
               ),
             ),
